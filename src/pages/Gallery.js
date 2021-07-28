a196fe12307a529d51c96aca782de9736f336c4b
inz2020 <>
2021-07-28 21:41:20 +0200

    Ajout d'une barre de recherche par ville dans la page gallery. Puis gestion de la pagination et affichage des photos en fonction du numéro de la page cliqué

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -12,24 +12,39 @@ class Gallery extends Component {
       currentPage: 1,
       //Nbre d'enregistrements par page
       pageSize: 10,
-      currentKeyword: 'rouen',
+      currentKeyword: '',
+      totalPages: 1,
+      pages: [],
     }
   }
   //Une fois que le composant est chargé
   componentDidMount() {
-    this.getHits()
+    // this.getHits()
   }
 
   //Au chargement du composant, on affiche ces objets
   getHits() {
     let url =
-      'https://pixabay.com/api/?key=21188057-c9c4d5022d25eb7c2bf09d819&q=rouen'
+      'https://pixabay.com/api/?key=21188057-c9c4d5022d25eb7c2bf09d819&q=' +
+      this.state.currentKeyword +
+      '&page=' +
+      this.state.currentPage +
+      '&per_page=' +
+      this.state.pageSize
     axios
       .get(url)
       .then((res) => {
         console.log(res)
+        //Calcul de totalPages
+        let tp =
+          res.data.totalHits % this.state.pageSize === 0
+            ? res.data.totalHits / this.state.pageSize
+            : 1 + res.data.totalHits / this.state.pageSize
+
         this.setState({
           hits: res.data.hits,
+          totalPages: tp,
+          pages: new Array(tp).fill(this.state.currentPage),
         })
       })
       .catch((err) => {
@@ -37,10 +52,52 @@ class Gallery extends Component {
       })
   }
 
+  setKeyword = (e) => {
+    this.setState({
+      currentKeyword: e.target.value,
+    })
+  }
+  //Fonction de recherche par ville
+  search = (e) => {
+    e.preventDefault()
+    this.getHits()
+  }
+  // Une fois que le state change , on fait appel à la fonction getHits. En effet, setState est une fonction asynchrone.
+  //Donc, ici getHits n'est appelé que si setState est terminée
+  gotoPage = (i) => {
+    this.setState(
+      {
+        currentPage: i,
+      },
+      () => this.getHits(),
+    )
+    console.log(this.state.currentPage)
+  }
+
   render() {
     return (
       <div>
-        <div className="row">
+        {/* Formulaire de recherche */}
+        <form onSubmit={this.search}>
+          {/* <div>{this.state.currentKeyword}</div> */}
+          <div className="row m-2 p-2">
+            <div className="col">
+              <input
+                className="form-control"
+                type="text"
+                placeholder=" type the name of the town you want to obtain its pictures"
+                value={this.state.currentKeyword}
+                onChange={this.setKeyword}
+              />
+            </div>
+            <div className="col-auto">
+              <button className="btn btn-success" type="submit">
+                Rechercher
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="row m-2 p-2">
           {this.state.hits.map((hit) => (
             <div key={hit.id} className="col-md-5 ">
               <div className="card">
@@ -58,6 +115,24 @@ class Gallery extends Component {
               </div>
             </div>
           ))}
+        </div>
+
+        <div>
+          <ul className="nav nav-pills">
+            {this.state.pages.map((v, i) => (
+              <li key={i.id}>
+                {/* i est l'index de la page, commençant par 0 on ajoute 1. La page courante est colorée par bleu */}
+                <a
+                  className={
+                    this.state.currentPage === i + 1 ? 'btn btn-primary' : 'btn'
+                  }
+                  onClick={() => this.gotoPage(i + 1)}
+                >
+                  {i + 1}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     )
